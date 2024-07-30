4e89c4b567cccea37637e1a87cd20a991314d80f
Eric Batlle <eric.batlle@tripledotstudios.com>
2024-07-30 15:29:08 +0200

    Initial Commit

---
+++ Assets/Scripts/Tests/Editor/FizzBuzzShould.cs
@@ -0,0 +1,30 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace Tests.Editor
+{
+    [TestFixture]
+    public class FizzBuzzShould
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void Return_Same_Number(int input)
+        {
+            //arrange
+            var fizzBuzz = new FizzBuzz();
+            //act
+            var result = fizzBuzz.Execute(input);
+            //assert
+            Assert.AreEqual(input.ToString(), result);
+        }
+    }
+
+    public class FizzBuzz
+    {
+        public string Execute(int input)
+        {
+            return input.ToString();
+        }
+    }
+}
