de97dded55df41780546334f85d3ef67411cd103
Mateusz Zawisza <mateusz.zawisza@tripledotstudios.com>
2024-08-02 16:18:25 +0200

    Stage 2 completed

--- Assets/Scripts/FizzBuzz.cs
+++ Assets/Scripts/FizzBuzz.cs
@@ -3,17 +3,32 @@ public class FizzBuzz
     public string GetElement(int value)
     {
         var result = string.Empty;
-            
-        if (value % 3 == 0)
+
+        if (value % 3 == 0 || HasGivenDigitInIt(value, 3))
         {
             result += "Fizz";
         }
-            
-        if (value % 5 == 0)
+
+        if (value % 5 == 0 || HasGivenDigitInIt(value, 5))
         {
             result += "Buzz";
         }
 
         return result == string.Empty ? value.ToString() : result;
+    }
+
+    private bool HasGivenDigitInIt(int value, int digit)
+    {
+        while (value != 0)
+        {
+            if (value % 10 == digit)
+            {
+                return true;
+            }
+
+            value /= 10;
+        }
+
+        return false;
     }
 }
--- Assets/Scripts/Tests/Editor/FizzBuzzShould.cs
+++ Assets/Scripts/Tests/Editor/FizzBuzzShould.cs
@@ -7,13 +7,13 @@ namespace Tests.Editor
     public class FizzBuzzShould
     {
         private FizzBuzz fizzBuzz;
-        
+
         [SetUp]
         public void SetUp()
         {
             fizzBuzz = new FizzBuzz();
         }
-        
+
         [TestCase(1)]
         [TestCase(2)]
         public void Print_The_Same_Number(int input)
@@ -46,5 +46,32 @@ namespace Tests.Editor
             Assert.AreEqual("FizzBuzz", value);
         }
 
+        [TestCase(13)]
+        public void Print_Fizz_When_Has_Three_In_It(int input)
+        {
+            var value = fizzBuzz.GetElement(input);
+            Assert.AreEqual("Fizz", value);
+        }
+
+        [TestCase(52)]
+        public void Print_Buzz_When_Has_Five_In_It(int input)
+        {
+            var value = fizzBuzz.GetElement(input);
+            Assert.AreEqual("Buzz", value);
+        }
+
+        [TestCase(53)]
+        public void Print_FizzBuzz_When_Has_Three_And_Has_Five_In_It(int input)
+        {
+            var value = fizzBuzz.GetElement(input);
+            Assert.AreEqual("FizzBuzz", value);
+        }
+
+        [TestCase(51)]
+        public void Print_FizzBuzz_When_Divisble_By_Three_And_Has_Five_In_It(int input)
+        {
+            var value = fizzBuzz.GetElement(input);
+            Assert.AreEqual("FizzBuzz", value);
+        }
     }
 }
